cf3afd69fd5826c3b90579dddd48dc1a923fcb8d
Aleksa Kundačina <aleksa.kundacina@energosoft.rs>
2022-10-12 12:16:26 +0200

    odradjeno

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
 let particleArray = [];
+let adjustX = 10;
+let adjustY = 5;
 
 const mouse = {
     x:null,
     y:null,
-    radius:250
+    radius:150
 }
 
 window.addEventListener("mousemove",function(event){
@@ -17,7 +20,7 @@ window.addEventListener("mousemove",function(event){
 
 ctx.fillStyle = "white";
 ctx.font = "30px Verdana";
-ctx.fillText("A",0,30);
+ctx.fillText("AK",0,30);
 
 const textCordinates = ctx.getImageData(0,0,100,100);
 
@@ -29,10 +32,11 @@ class Particle {
         this.baseX = x;
         this.baseY = y;
         this.density = (Math.random() * 40) + 5;
+        this.color = "white"
     }
 
     draw(){
-        ctx.fillStyle = "white";
+        ctx.fillStyle = this.color;
         ctx.beginPath();
         ctx.arc(this.x,this.y,this.size,0,Math.PI * 2);
         ctx.closePath();
@@ -55,20 +59,22 @@ class Particle {
         let directionY = forceDirectionY * force * this.density;
 
         if(distance < mouse.radius){
+            this.color = "red";
             this.x -= directionX;
             this.y -= directionY;
         }else{
             if(this.x !== this.baseX){
+                this.color = "white";
                 let dx = this.x - this.baseX;
                 this.x -= dx/10;
             }
             if(this.y !== this.baseY){
+                this.color = "white";
                 let dy = this.y - this.baseY;
                 this.y -= dy/10;
             }
         }
     }
-
 }
 
 function init(){
@@ -76,8 +82,10 @@ function init(){
     
     for(let y = 0, y2 = textCordinates.height; y< y2; y++){
         for(let x = 0, x2 = textCordinates.width; x < x2; x++){
-            if(textCordinates.data[1] > 128){
-                
+            if(textCordinates.data[(y * 4 * textCordinates.width) + (x * 4) + 3] > 128){
+                let positionX = x + adjustX;
+                let positionY = y + adjustY;
+                particleArray.push(new Particle(positionX * 20,positionY * 20));
             }
         }
     }
@@ -85,15 +93,33 @@ function init(){
 }
 init();
 
-console.log(particleArray);
-
 function animate(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
     for(let i = 0; i < particleArray.length; i++){
         particleArray[i].draw();
         particleArray[i].update();
     }
+    connect();
     requestAnimationFrame(animate);
 }
-
 animate();
+
+function connect(){
+    let opacityValue = 1;
+    for(let a = 0; a< particleArray.length; a++){
+        for(let b = a; b < particleArray.length; b++){
+            let dx = particleArray[a].x - particleArray[b].x
+            let dy = particleArray[a].y - particleArray[b].y
+            let distance = Math.sqrt(dx * dx + dy * dy);
+            if(distance < 50){
+                opacityValue = 1 - (distance/50);
+                ctx.strokeStyle = "rgba(255,105,105," + opacityValue + ")";
+                ctx.lineWidth = 2;
+                ctx.beginPath();
+                ctx.moveTo(particleArray[a].x,particleArray[a].y);
+                ctx.lineTo(particleArray[b].x,particleArray[b].y);
+                ctx.stroke();
+            }
+        }
+    }
+}
